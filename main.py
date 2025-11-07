a810a59604df574cc1e1f483e7d5f60f77db85b0
1tc-ladlev <ladanyilevente5@gmail.com>
2025-11-07 10:13:41 +0100

    1.feladat

---
+++ main.py
@@ -0,0 +1,17 @@
+#1. Adatok beolvasa listaban
+adatok=[] 
+with open ("data.txt", "r", encoding="utf-8") as fin:
+    for sor in fin:
+        adatok.append(int(sor))
+print(adatok)
+
+#2. Adatok átlaga
+atlag=sum(adatok)/len(adatok)
+print("Átlaga:" (atlag))
+#3. dontsuk el hogy volt e 4
+volt_e=
+#4. keressuk meg volt e 5
+#5.hany darab kilences volt?
+#6. Mennyi a legnagyobb beirt szam?
+#7. Hanyadik indexen van a legkisebb elem?
+#8. paros szamok kiirasa 
